419c64dfd1f1629e7a20cda14e35ccd4ca74f054
Jackykit123 <jackykit19970402@gmail.com>
2020-05-25 03:14:55 +0800

    fixing external link

--- src/Components/Menu/menu.tsx
+++ src/Components/Menu/menu.tsx
@@ -8,19 +8,36 @@ interface MenuProps {
 
 export default function menu({ menuList }: MenuProps): JSX.Element {
     const createMenu = (menulist: Menu | string): JSX.Element[] =>
-        Object.entries(menulist).map(menuItem => (
-            <li key={menuItem[0]}>
-                {typeof menuItem[1].path === 'string' ? (
-                    <Link to={menuItem[1].path}> {menuItem[0]}</Link>
-                ) : (
-                    <span>{menuItem[0]}</span>
-                )}
-                {typeof menuItem[1].path === 'string' ? (
-                    ''
-                ) : (
-                    <ul>{createMenu(menuItem[1])}</ul>
-                )}
-            </li>
-        ));
+        Object.entries(menulist).map(menuItem => {
+            let returnJsx;
+            switch (true) {
+                case typeof menuItem[1].path === 'string' &&
+                    menuItem[1].external:
+                    returnJsx = (
+                        <a
+                            target='_blank'
+                            rel='noopener noreferrer'
+                            href={menuItem[1].path}
+                        >
+                            {menuItem[0]}
+                        </a>
+                    );
+                    break;
+                case typeof menuItem[1].path === 'string' &&
+                    !menuItem[1].external:
+                    returnJsx = (
+                        <Link to={menuItem[1].path}>{menuItem[0]}</Link>
+                    );
+                    break;
+                default:
+                    returnJsx = (
+                        <>
+                            <span>{menuItem[0]}</span>
+                            <ul>{createMenu(menuItem[1])}</ul>
+                        </>
+                    );
+            }
+            return <li key={menuItem[0]}>{returnJsx}</li>;
+        });
     return <ul className='menu'>{createMenu(menuList)}</ul>;
 }
