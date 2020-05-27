68811487388c4e692ac803ea32bd383dc2683f25
Jackykit123 <jackykit19970402@gmail.com>
2020-05-27 19:02:40 +0800

    fixing menu structure

--- src/Screens/App.tsx
+++ src/Screens/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'reset-css';
 import './App.less';
@@ -7,7 +7,32 @@ import { fetchDecks, fetchDices } from './fetchData';
 import Header from './Header & Footer/header';
 import Footer from './Header & Footer/footer';
 import NoMatch from './NoMatch/NoMatch';
-import { menu } from './menuConfig';
+import { menu as menuConfig, Menu } from './menuConfig';
+
+const mapRouter = (
+    menu: Menu[]
+): (JSX.Element | FunctionComponent<Route> | null)[] =>
+    menu
+        .map(item => {
+            if (item.childNode) {
+                if (item.component) {
+                    return [...mapRouter(item.childNode), item.component];
+                }
+                return mapRouter(item.childNode);
+            }
+            if (item.component) {
+                return (
+                    <Route
+                        key={`Route-path-${item.path}`}
+                        exact
+                        path={item.path}
+                        component={item.component}
+                    />
+                );
+            }
+            return null;
+        })
+        .flat();
 
 export default function App(): JSX.Element {
     const dispatch = useDispatch();
@@ -17,14 +42,7 @@ export default function App(): JSX.Element {
         <Router>
             <Header />
             <Switch>
-                {menu.map(item => (
-                    <Route
-                        key={`Route-path-${item.path}`}
-                        exact
-                        path={item.path}
-                        component={item.component}
-                    />
-                ))}
+                {mapRouter(menuConfig)}
                 <Route component={NoMatch} />
             </Switch>
             <Footer />
