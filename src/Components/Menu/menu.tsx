10cd76e4504758f53d7cf021274bdaa93ad1c4b9
Jackykit123 <jackykit19970402@gmail.com>
2020-06-01 15:19:41 +0800

    added tab function for accessibility

--- src/Components/Menu/menu.tsx
+++ src/Components/Menu/menu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Menu as MenuProp } from '../../Screens/Misc/menuConfig';
 
@@ -7,15 +7,31 @@ export default function Menu(props: {
     className?: string | undefined;
 }): JSX.Element {
     const { menuList, className } = props;
+    const [focusedTab, setFocusedTab] = useState(-1);
     const location = useLocation();
-    console.log(location.pathname);
-    const createMenu = (menulist: MenuProp[]): (JSX.Element | null)[] => {
-        return menulist.map((menu: MenuProp) => {
+
+    const keyup = (evt: KeyboardEvent): void => {
+        const target = evt.target as HTMLElement;
+        if (!target.className.split(' ').includes('menu-item')) {
+            setFocusedTab(-1);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('keyup', keyup);
+        return (): void => document.removeEventListener('keyup', keyup);
+    });
+    const createMenu = (
+        menulist: MenuProp[],
+        depth = 0
+    ): (JSX.Element | null)[] => {
+        return menulist.map((menu: MenuProp, i) => {
             let anchor;
             switch (true) {
                 case menu.external:
                     anchor = (
                         <a
+                            className='menu-item'
                             target='_blank'
                             rel='noopener noreferrer'
                             href={menu.path}
@@ -25,19 +41,30 @@ export default function Menu(props: {
                     );
                     break;
                 case !menu.external && menu.path === location.pathname:
-                    anchor = <span className='active'>{menu.name}</span>;
+                    anchor = (
+                        <span className='active menu-item'>{menu.name}</span>
+                    );
                     break;
                 case !menu.external && menu.path !== undefined:
-                    anchor = <Link to={String(menu.path)}>{menu.name}</Link>;
+                    anchor = (
+                        <Link className='menu-item' to={String(menu.path)}>
+                            {menu.name}
+                        </Link>
+                    );
                     break;
                 default:
-                    anchor = <span>{menu.name}</span>;
+                    anchor = <span className='menu-item'>{menu.name}</span>;
             }
             const childNode = menu.childNode ? (
-                <ul>{createMenu(menu.childNode)}</ul>
+                <ul>{createMenu(menu.childNode, depth + 1)}</ul>
             ) : null;
             return (
-                <li key={`menu-${className}-${menu.name}`}>
+                <li
+                    key={`menu-${className}-${menu.name}`}
+                    tabIndex={depth === 0 ? 0 : -1}
+                    className={`menu-item ${focusedTab === i ? 'focused' : ''}`}
+                    onFocus={(): void => setFocusedTab(i)}
+                >
                     {anchor}
                     {childNode}
                 </li>
