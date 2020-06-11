b71e3d6af428f6999582fc0dff62707a4fe015ea
Jackykit123 <jackykit19970402@gmail.com>
2020-06-11 13:56:12 +0800

    updated dice mechanics info

---
+++ src/Screens/Cal/DPS/dps.tsx
@@ -0,0 +1,239 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Main from '../../../Components/Main/main';
+import Error from '../../../Components/Error/error';
+import LoadingScreen from '../../../Components/Loading/loading';
+import Dice from '../../../Components/Dice/dice';
+import { RootState } from '../../../Misc/Redux Storage/store';
+import { clearError, fetchDices } from '../../../Misc/fetchData';
+import '../cal.less';
+
+export default function GearsCaculator(): JSX.Element {
+    const dispatch = useDispatch();
+    const selection = useSelector(
+        (state: RootState) => state.fetchDicesReducer
+    );
+    const { error, dices } = selection;
+    const [filter, setFilter] = useState({
+        class: 5,
+        level: 1,
+        chain: 0,
+        pip: 1,
+        crit: 111,
+    });
+    let jsx = <div />;
+    const data = dices?.find(dice => dice.name === 'Gears');
+    const isInvalidCrit =
+        !Number.isInteger(filter.crit) ||
+        filter.crit < 111 ||
+        filter.crit > 2036;
+    const isInvalidPip =
+        !Number.isInteger(filter.pip) || filter.pip < 1 || filter.pip > 7 * 15;
+    const isInvalidChain =
+        filter.pip < filter.chain + 1 || filter.pip > (filter.chain + 1) * 7;
+    const invalidInput = isInvalidCrit || isInvalidChain || isInvalidPip;
+
+    if (data) {
+        jsx = (
+            <>
+                <p>
+                    This is a calculator for calculating the dps for generic
+                    dice that have no special interactions or effect and instead
+                    they attack plain basic attack damage.
+                </p>
+                <p>
+                    We have only included damage dealer dice like typhoon. Value
+                    dice or slow dice dps is ignored as their dps in negligible.
+                    For dice with special interaction like Solar, Gear or Combo,
+                    please refer to the tab for more information.
+                </p>
+                <p>
+                    Moreover, dice that deals AOE damage is not included. This
+                    includes Landmine, Thorn, Infect, etc. Basically, only meta
+                    dps dice are included. You can request for more calculator
+                    for other dice and your suggestion may be considered and we
+                    will make more calculators.
+                </p>
+                <div className='divisor' />
+                <div className='dice-container'>
+                    <div>
+                        <Dice dice='Gears' />
+                        <h3 className='desc'>{data.desc}</h3>
+                    </div>
+                </div>
+                <form className='filter'>
+                    <label htmlFor='class'>
+                        <span>Class :</span>
+                        <select
+                            name='class'
+                            onChange={(
+                                evt: React.ChangeEvent<HTMLSelectElement>
+                            ): void => {
+                                filter.class = Number(evt.target.value);
+                                setFilter({ ...filter });
+                            }}
+                        >
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                            <option>9</option>
+                            <option>10</option>
+                            <option>11</option>
+                            <option>12</option>
+                            <option>13</option>
+                            <option>14</option>
+                            <option>15</option>
+                        </select>
+                    </label>
+                    <label htmlFor='level'>
+                        <span>Level :</span>
+                        <select
+                            name='level'
+                            onChange={(
+                                evt: React.ChangeEvent<HTMLSelectElement>
+                            ): void => {
+                                filter.level = Number(evt.target.value);
+                                setFilter({ ...filter });
+                            }}
+                        >
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </label>
+                    <label htmlFor='chain'>
+                        <span>Chain Length :</span>
+                        <select
+                            name='chain'
+                            className={isInvalidChain ? 'invalid' : ''}
+                            onChange={(
+                                evt: React.ChangeEvent<HTMLSelectElement>
+                            ): void => {
+                                filter.chain = Number(evt.target.value);
+                                setFilter({ ...filter });
+                            }}
+                        >
+                            <option>0</option>
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                            <option>9</option>
+                            <option>10</option>
+                            <option>11</option>
+                            <option>12</option>
+                            <option>13</option>
+                            <option>14</option>
+                        </select>
+                    </label>
+                    <label htmlFor='pip'>
+                        <span>Total Gears Pip:</span>
+                        <input
+                            type='textbox'
+                            name='pip'
+                            defaultValue={1}
+                            className={
+                                isInvalidPip || isInvalidChain ? 'invalid' : ''
+                            }
+                            onChange={(
+                                evt: React.ChangeEvent<HTMLInputElement>
+                            ): void => {
+                                const val = Number(evt.target.value);
+                                filter.pip = val;
+                                setFilter({ ...filter });
+                            }}
+                        />
+                    </label>
+                    <label htmlFor='crit dmg'>
+                        <span>Crit%:</span>
+                        <input
+                            type='textbox'
+                            name='crit dmg'
+                            defaultValue={111}
+                            className={isInvalidCrit ? 'invalid' : ''}
+                            onChange={(
+                                evt: React.ChangeEvent<HTMLInputElement>
+                            ): void => {
+                                const val = Number(evt.target.value);
+                                filter.crit = val;
+                                setFilter({ ...filter });
+                            }}
+                        />
+                    </label>
+                </form>
+                {isInvalidChain ? (
+                    <span className='invalid-warning'>
+                        {`Invalid pip amount, ${
+                            filter.pip > filter.chain
+                                ? 'too many gears pips comparing to the chain length.'
+                                : 'chain length should be smaller than total gears pip.'
+                        }`}
+                    </span>
+                ) : (
+                    ''
+                )}
+                {isInvalidPip ? (
+                    <span className='invalid-warning'>
+                        Invalid Pip Count Input! Acceptable input is{' '}
+                        <strong>1-105</strong>.
+                    </span>
+                ) : (
+                    ''
+                )}
+                {isInvalidCrit ? (
+                    <span className='invalid-warning'>
+                        Invalid Crit% Input! Acceptable input is{' '}
+                        <strong>111-2036</strong>.
+                    </span>
+                ) : (
+                    ''
+                )}
+                <div className='divisor' />
+                <div className='result'>
+                    <div className='dmg'>
+                        <span>Damage per Gears pip :</span>
+                        <input
+                            type='textbox'
+                            className={invalidInput ? 'invalid' : ''}
+                            // value={invalidInput ? 'Check Input' : dmgPerPip}
+                            disabled
+                        />
+                    </div>
+                    <div className='dps'>
+                        <span>Total DPS :</span>
+                        <input
+                            type='textbox'
+                            className={invalidInput ? 'invalid' : ''}
+                            // value={invalidInput ? 'Check Input' : dps}
+                            disabled
+                        />
+                    </div>
+                </div>
+            </>
+        );
+    } else if (error) {
+        jsx = (
+            <Error
+                error={error}
+                retryFn={(): void => {
+                    clearError(dispatch);
+                    fetchDices(dispatch);
+                }}
+            />
+        );
+    } else {
+        jsx = <LoadingScreen />;
+    }
+    return (
+        <Main title='Gears DPS Calculator' className='gears-dmg-cal cal'>
+            {jsx}
+        </Main>
+    );
+}
