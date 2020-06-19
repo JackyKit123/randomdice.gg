83d482b015c68177e655f982d882f4a752fc7a5b
Jackykit123 <jackykit19970402@gmail.com>
2020-06-19 21:21:02 +0800

    removed reduce from fetch Alt and fetch Arena

--- src/Misc/Redux Storage/store.ts
+++ src/Misc/Redux Storage/store.ts
@@ -2,18 +2,14 @@ import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import fetchDecksReducer from './Fetch Decks/reducer';
 import fetchDicesReducer from './Fetch Dices/reducer';
-import fetchAltsReducer from './Fetch Alt/reducer';
-import fetchArenaReducer from './Fetch Arena/reducer';
 import filterReducer from './Deck Filter/reducer';
 import fetchGAPIresponseFormReducer from './Google API Fetch Response Form/reducers';
 
 const rootReducer = combineReducers({
-    fetchArenaReducer,
     fetchDecksReducer,
     fetchDicesReducer,
     filterReducer,
     fetchGAPIresponseFormReducer,
-    fetchAltsReducer,
 });
 export type RootState = ReturnType<typeof rootReducer>;
 export const store = createStore(
