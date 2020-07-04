affe269d33b23a59e71f8d8e317204f52cd636a7
Jackykit123 <jackykit19970402@gmail.com>
2020-07-05 04:48:09 +0800

    updated ad unit placement login

--- src/Screens/Wiki/Boss/boss.tsx
+++ src/Screens/Wiki/Boss/boss.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ReactHtmlParser from 'react-html-parser';
 import { sanitize } from 'dompurify';
@@ -10,14 +10,29 @@ import { RootState } from '../../../Misc/Redux Storage/store';
 import { CLEAR_ERRORS } from '../../../Misc/Redux Storage/Fetch Firebase/types';
 import { fetchWiki } from '../../../Misc/Firebase/fetchData';
 import './boss.less';
+import { WikiContent } from '../../../Misc/Redux Storage/Fetch Firebase/Wiki/types';
 
 export default function BossGuide(): JSX.Element {
     const dispatch = useDispatch();
     const { wiki, error } = useSelector(
         (state: RootState) => state.fetchWikiReducer
     );
+    const [bossInfo, setBossInfo] = useState<WikiContent['boss']>();
+
+    useEffect(() => {
+        if (wiki) {
+            wiki.boss.splice(Math.floor(wiki.boss.length / 2), 0, {
+                id: -1,
+                name: 'ad',
+                img: 'ad',
+                desc: 'ad',
+            });
+            setBossInfo(wiki.boss);
+        }
+    }, [wiki]);
+
     let jsx;
-    if (wiki) {
+    if (bossInfo) {
         jsx = (
             <>
                 <p>On here you will find information about Game Boss.</p>
@@ -31,22 +46,30 @@ export default function BossGuide(): JSX.Element {
                     contribute to the increment of the boss health. So try not
                     to overwhelm your board with too many mobs.
                 </p>
-                <div className='divisor' />
-                <AdUnit unitId='227378933' dimension='300x250' />
-                <AdUnit unitId='219055766' dimension='970x90' />
                 <section className='boss-list'>
-                    {wiki.boss.map(boss => (
-                        <Fragment key={boss.name}>
-                            <div className='divisor' />
-                            <div className='boss'>
-                                <figure>
-                                    <img src={boss.img} alt={boss.name} />
-                                </figure>
-                                <h3>{boss.name}</h3>
-                                {ReactHtmlParser(sanitize(boss.desc))}
-                            </div>
-                        </Fragment>
-                    ))}
+                    {bossInfo.map(boss =>
+                        boss.img === 'ad' ? (
+                            <Fragment key='ad'>
+                                <hr className='divisor' />
+                                <AdUnit
+                                    unitId='227378933'
+                                    dimension='300x250'
+                                />
+                                <AdUnit unitId='219055766' dimension='970x90' />
+                            </Fragment>
+                        ) : (
+                            <Fragment key={boss.name}>
+                                <div className='divisor' />
+                                <div className='boss'>
+                                    <figure>
+                                        <img src={boss.img} alt={boss.name} />
+                                    </figure>
+                                    <h3>{boss.name}</h3>
+                                    {ReactHtmlParser(sanitize(boss.desc))}
+                                </div>
+                            </Fragment>
+                        )
+                    )}
                 </section>
             </>
         );
