c57f741d4288e0d987f3e0a2b35dafd6ed148490
Jackykit123 <jackykit19970402@gmail.com>
2020-07-09 22:19:36 +0800

    removed usused useEffect

--- src/Components/Ad Unit/ad.tsx
+++ src/Components/Ad Unit/ad.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import 'firebase/database';
 import { detected } from 'adblockdetect';
