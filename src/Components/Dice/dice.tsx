9a6fde4afbb3d2fea27e8f9206e713c80697e6b4
Jackykit123 <jackykit19970402@gmail.com>
2020-09-03 07:31:55 +0800

    scaling down non legendary dice for better ui

--- src/Components/Dice/dice.tsx
+++ src/Components/Dice/dice.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../Misc/Redux Storage/store';
+import './dice.less';
 
 export default function Dice({ dice }: { dice: number | string }): JSX.Element {
     const { dices } = useSelector(
@@ -10,6 +11,7 @@ export default function Dice({ dice }: { dice: number | string }): JSX.Element {
     return (
         <img
             alt={`dice ${targetDice?.name || '?'}`}
+            data-dice-rarity={targetDice?.rarity || '?'}
             src={
                 targetDice?.img ||
                 'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/Dice%20Images%2FEmpty.png?alt=media&token=193f9435-4c38-4ef0-95cd-317d9fbe6efe'
