2b435fe5c232cf12f111748a3c15391f89f5a391
Jackykit123 <jackykit19970402@gmail.com>
2020-09-03 08:41:39 +0800

    updated more images

--- src/Misc/replaceTextWithImg.ts
+++ src/Misc/replaceTextWithImg.ts
@@ -1,4 +1,4 @@
-import { Dices } from './Redux Storage/Fetch Firebase/Dices/types';
+import { Dice, Dices } from './Redux Storage/Fetch Firebase/Dices/types';
 
 export default function replaceTextWithImgTag(
     text: string,
@@ -7,12 +7,14 @@ export default function replaceTextWithImgTag(
     return text
         .replace(/{Dice:\w*( \w*)*}/g, match => {
             const diceName = match.replace('{Dice:', '').replace('}', '');
-            const imgUrl = dices?.find(dice => dice.name === diceName)?.img;
+            const { img, rarity } = dices?.find(
+                dice => dice.name === diceName
+            ) as Dice;
             const nullDiceUrl =
                 'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/Dice%20Images%2FEmpty.png?alt=media&token=193f9435-4c38-4ef0-95cd-317d9fbe6efe';
-            return `<img src="${imgUrl || nullDiceUrl}" alt="${
-                imgUrl ? `dice ${diceName}` : 'dice ?'
-            }" />`;
+            return `<img src="${img || nullDiceUrl}" alt="${
+                img ? `dice ${diceName}` : 'dice ?'
+            }" data-dice-rarity=${rarity || '?'} />`;
         })
         .replace(
             /({Gold}|{Diamond}|{Common Dice}|{Rare Dice}|{Unique Dice}|{Legendary Dice})/g,
@@ -22,27 +24,27 @@ export default function replaceTextWithImgTag(
                 switch (match) {
                     case '{Gold}':
                         src =
-                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FGold.png?alt=media&token=bbde2bc8-21e8-4b51-9e72-c2bd4914d371';
+                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FGold.png?alt=media&token=28b9c744-31fd-4f1f-b640-a63d19c6f8d3';
                         break;
                     case '{Diamond}':
                         src =
-                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FDiamond.png?alt=media&token=d151bac0-7979-4f73-a140-ad4b8aa63600';
+                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FDiamond.png?alt=media&token=5d87897b-396a-46f5-9721-9431467ba1ad';
                         break;
                     case '{Common Dice}':
                         src =
-                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FCommon%20Dice.png?alt=media&token=a02c6e1e-f084-40b7-b147-dd0e1855d9af';
+                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FCommon%20Dice.png?alt=media&token=d1327589-1148-4135-986c-64351f5bcd5f';
                         break;
                     case '{Rare Dice}':
                         src =
-                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FRare%20Dice.png?alt=media&token=42a77dc1-ccfe-41de-ba36-9ff775269b0a';
+                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FRare%20Dice.png?alt=media&token=c76c87f8-1e1c-495b-9b0c-e96cef16909c';
                         break;
                     case '{Unique Dice}':
                         src =
-                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FUnique%20Dice.png?alt=media&token=b00aaf25-dafe-4020-b88e-b61fe998670c';
+                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FUnique%20Dice.png?alt=media&token=ecbda29b-513d-4789-ac1a-73248202679c';
                         break;
                     case '{Legendary Dice}':
                         src =
-                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FLegendary%20Dice.png?alt=media&token=2271d03d-5fab-40c0-a9f4-19803688d9e9';
+                            'https://firebasestorage.googleapis.com/v0/b/random-dice-web.appspot.com/o/General%20Images%2FLegendary%20Dice.png?alt=media&token=7e5835d0-1d8f-416e-8ac1-c2b80d78e3b5';
                         break;
                     default:
                         src = '';
