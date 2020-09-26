30427f39d37197f51294ed053bd03736375ac73f
Jackykit123 <jackykit19970402@gmail.com>
2020-09-26 21:15:05 +0800

    remove duplicated extend

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,6 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
         'prettier/@typescript-eslint',
-        'plugin:prettier/recommended',
     ],
     overrides: [
         {
