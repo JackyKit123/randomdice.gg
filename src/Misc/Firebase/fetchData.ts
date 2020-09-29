8e6de05c12cbcce89fd846b8215e2f89113e0f72
Jackykit123 <jackykit19970402@gmail.com>
2020-09-29 16:55:25 +0800

    enhanced the localStorage caching logic

--- src/Misc/Firebase/fetchData.ts
+++ src/Misc/Firebase/fetchData.ts
@@ -19,12 +19,11 @@ async function fetch(
     dispatch: ReturnType<typeof useDispatch>,
     successAction: string,
     errorAction: string,
-    dbPath: string,
-    localStorageKey: string
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    key: string,
+    forceFetch?: true
 ): Promise<void> {
     // apply local storage cache before fetching database
-    const localCache = localStorage.getItem(localStorageKey);
+    const localCache = localStorage.getItem(key);
     if (localCache) {
         try {
             dispatch({
@@ -33,56 +32,59 @@ async function fetch(
             });
         } catch (err) {
             // error at JSON.parse, prob corrupted JSON, removing item and proceed to fetch database
-            localStorage.removeItem(localStorageKey);
+            localStorage.removeItem(key);
         }
     }
 
     try {
+        if (forceFetch) {
+            const data = await database.ref(`/${key}`).once('value');
+            dispatch({ type: successAction, payload: data.val() });
+            localStorage.setItem(key, JSON.stringify(data.val()));
+            return;
+        }
+
         // check database path last updated time
-        const localVersion = localStorage.getItem('last_updated');
-        let parsedJson;
+        const localVersionJson = localStorage.getItem('last_updated');
+        let localVersion;
 
         try {
-            parsedJson = JSON.parse(localVersion as string);
+            localVersion = JSON.parse(localVersionJson as string);
         } catch (err) {
             localStorage.removeItem('last_updated');
         }
 
-        const remoteVersion = (
-            await database.ref('/last_updated').once('value')
+        const remoteKeyVersion = (
+            await database.ref(`/last_updated/${key}`).once('value')
         ).val();
-
         // localVersion match database version, no need to refetch
-        if (
-            parsedJson &&
-            remoteVersion[dbPath.replace(/^\//, '')] &&
-            parsedJson[dbPath.replace(/^\//, '')] &&
-            remoteVersion[dbPath.replace(/^\//, '')] ===
-                parsedJson[dbPath.replace(/^\//, '')]
-        ) {
+        if (remoteKeyVersion && remoteKeyVersion === localVersion?.[key]) {
             return;
         }
 
         // localVersion doesn't match database version, proceed to fetch database
-        const res = await database.ref(dbPath).once('value');
-        dispatch({ type: successAction, payload: res.val() });
+        const remoteVersion = (
+            await database.ref(`/last_updated`).once('value')
+        ).val();
+        const data = await database.ref(`/${key}`).once('value');
+        dispatch({ type: successAction, payload: data.val() });
         localStorage.setItem('last_updated', JSON.stringify(remoteVersion));
-        localStorage.setItem(localStorageKey, JSON.stringify(res.val()));
+        localStorage.setItem(key, JSON.stringify(data.val()));
     } catch (err) {
         dispatch({ type: errorAction, payload: err });
     }
 }
 
 export function fetchDecks(dispatch: ReturnType<typeof useDispatch>): void {
-    fetch(dispatch, FETCH_DECKS.SUCCESS, FETCH_DECKS.FAIL, '/decks', 'decks');
+    fetch(dispatch, FETCH_DECKS.SUCCESS, FETCH_DECKS.FAIL, 'decks');
 }
 
 export function fetchDices(dispatch: ReturnType<typeof useDispatch>): void {
-    fetch(dispatch, FETCH_DICES.SUCCESS, FETCH_DICES.FAIL, '/dice', 'dices');
+    fetch(dispatch, FETCH_DICES.SUCCESS, FETCH_DICES.FAIL, 'dice');
 }
 
 export function fetchWiki(dispatch: ReturnType<typeof useDispatch>): void {
-    fetch(dispatch, FETCH_WIKI.SUCCESS, FETCH_WIKI.FAIL, '/wiki', 'wiki');
+    fetch(dispatch, FETCH_WIKI.SUCCESS, FETCH_WIKI.FAIL, 'wiki');
 }
 
 export function fetchDecksGuide(
@@ -92,56 +94,36 @@ export function fetchDecksGuide(
         dispatch,
         FETCH_DECKS_GUIDE.SUCCESS,
         FETCH_DECKS_GUIDE.FAIL,
-        '/decks_guide',
         'decks_guide'
     );
 }
 
 export function fetchCredit(dispatch: ReturnType<typeof useDispatch>): void {
-    fetch(
-        dispatch,
-        FETCH_CREDIT.SUCCESS,
-        FETCH_CREDIT.FAIL,
-        '/credit',
-        'credit'
-    );
+    fetch(dispatch, FETCH_CREDIT.SUCCESS, FETCH_CREDIT.FAIL, 'credit');
 }
 
 export function fetchUser(
     dispatch: ReturnType<typeof useDispatch>,
     uid: string
 ): void {
-    fetch(
-        dispatch,
-        FETCH_USER.SUCCESS,
-        FETCH_USER.FAIL,
-        `/users/${uid}`,
-        'user'
-    );
+    Object.keys(localStorage)
+        .filter(
+            keyName => keyName.match(/^users\/.*/) && keyName !== `users/${uid}`
+        )
+        .forEach(userCache => localStorage.removeItem(userCache));
+    fetch(dispatch, FETCH_USER.SUCCESS, FETCH_USER.FAIL, `users/${uid}`, true);
 }
 
 export function fetchNews(dispatch: ReturnType<typeof useDispatch>): void {
-    fetch(dispatch, FETCH_NEWS.SUCCESS, FETCH_NEWS.FAIL, '/news', 'news');
+    fetch(dispatch, FETCH_NEWS.SUCCESS, FETCH_NEWS.FAIL, 'news');
 }
 
 export function fetchPatreon(dispatch: ReturnType<typeof useDispatch>): void {
-    fetch(
-        dispatch,
-        FETCH_PATREON.SUCCESS,
-        FETCH_PATREON.FAIL,
-        '/patreon_list',
-        'patreon_list'
-    );
+    fetch(dispatch, FETCH_PATREON.SUCCESS, FETCH_PATREON.FAIL, 'patreon_list');
 }
 
 export function fetchCrit(dispatch: ReturnType<typeof useDispatch>): void {
-    fetch(
-        dispatch,
-        FETCH_CRIT.SUCCESS,
-        FETCH_CRIT.FAIL,
-        '/critData',
-        'critData'
-    );
+    fetch(dispatch, FETCH_CRIT.SUCCESS, FETCH_CRIT.FAIL, 'critData');
 }
 
 export default function fetchAll(
