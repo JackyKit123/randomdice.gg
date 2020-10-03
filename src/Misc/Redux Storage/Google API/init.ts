74239a45854806bda648dbee5e7915916cbf510a
Jackykit123 <jackykit19970402@gmail.com>
2020-10-03 13:11:17 +0800

    reverted google api intiation behaviour

--- src/Misc/Redux Storage/Google API/init.ts
+++ src/Misc/Redux Storage/Google API/init.ts
@@ -3,20 +3,25 @@ import { fetchYouTube } from './fetchData';
 import { FETCH_GOOGLE_API_FAIL } from './types';
 
 export default function init(dispatch: ReturnType<typeof useDispatch>): void {
-    window.gapi.load('client', async () => {
-        try {
-            await window.gapi.client.init({
-                apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-                discoveryDocs: [
-                    'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest',
-                ],
-            });
-            await fetchYouTube(dispatch);
-        } catch (err) {
-            dispatch({
-                type: FETCH_GOOGLE_API_FAIL,
-                payload: err.error,
-            });
-        }
-    });
+    const script = document.createElement('script');
+    script.src = 'https://apis.google.com/js/client.js';
+    script.onload = (): void => {
+        window.gapi.load('client', async () => {
+            try {
+                await window.gapi.client.init({
+                    apiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+                    discoveryDocs: [
+                        'https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest',
+                    ],
+                });
+                await fetchYouTube(dispatch);
+            } catch (err) {
+                dispatch({
+                    type: FETCH_GOOGLE_API_FAIL,
+                    payload: err.error,
+                });
+            }
+        });
+    };
+    document.body.append(script);
 }
