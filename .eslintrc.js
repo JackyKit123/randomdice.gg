f11df7fbdf0219637dca47e38e7f8288518cd782
JackyKit <jackykit19970402@gmail.com>
2021-09-22 19:20:53 -0700

    turn off jsx-indent rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,8 +43,8 @@ module.exports = {
     },
     rules: {
         'react/jsx-filename-extension': [1, { extensions: ['tsx'] }],
-        'react/jsx-indent': [2, 2],
-        'react/jsx-indent-props': [2, 2],
+        'react/jsx-indent': 'off',
+        'react/jsx-indent-props': 'off',
         'prettier/prettier': 'error',
         'import/extensions': [
             'error',
